9f811455103f12d57b7c4ea23b22b1997515afed
SERRANO GIL <msg92s@carm.es>
2025-03-24 13:05:46 +0100

    Agregar contenido a index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
+      <h1>¡Bienvenido a Vacunas API!</h1>
+      <p>Esta es la página principal de tu proyecto en Next.js.</p>
+    </div>
+  );
+}
+
